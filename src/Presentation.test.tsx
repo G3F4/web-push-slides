8221b12d758acdd61e2a4108f266384be19f3b6a
G3F4 <kamil.grabek@gmail.com>
2020-01-05 21:10:30 +0100

    udrożnienie testów i włączenie na CI

--- src/Presentation.test.tsx
+++ src/Presentation.test.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Presentation from './Presentation';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+window.Notification = class Notification {
+  permission: NotificationPermission;
+
+  constructor() {
+    this.permission = 'default';
+  }
+
+  async requestPermission() {
+    // eslint-disable-next-line no-undef
+    return Promise.resolve('denied');
+  }
+};
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
